1dd853cdd27a4ea4ce5a879fbb3491c76878a104
Francois-Xavier Fabre <fxfabre@users.noreply.github.com>
2024-01-09 20:45:38 +0100

    Text to speech API

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # doc_to_speech
-From word / pdf to audio
+Book reader : From word / pdf to audio
+
+Chaque sous dossier est un projet / une API indépendante :
+- tesseract_fast_api : en developpement
+- text_to_speech : TTS avec suno / bark
+
+
+## Exemple d'utilisation
+
+Lancement de l'API :
+- `docker compose up --build tts-api`
+
+Generation de la voix :
+```python
+import requests
+import scipy
+import numpy as np
+
+body = {
+    "language": "fr",
+    "text": "Ceci est un test de génération de parole."
+}
+response = requests.post("http://localhost:5000/api/v1/tts", json=body)
+
+scipy.io.wavfile.write(
+    "sample.wav",
+    rate=response.json()["rate"],
+    data=np.array(response.json()["data"])
+)
+```
---
+++ main.py
@@ -0,0 +1,50 @@
+import posixpath
+
+import numpy as np
+import os
+import requests
+import scipy
+from pathlib import Path
+
+API_BASE_URL = "http://192.168.1.90:5000/api/v1"
+
+
+def main():
+    folder_articles = Path("articles")
+    folder_audio_out = Path("audio_out")
+
+    for root, sub_dirs, files in os.walk(folder_articles):
+        relative_dir = Path(root).relative_to(folder_articles)
+
+        for file_name in files:
+            text_file = os.path.join(root, file_name)
+            audio_file = folder_audio_out / relative_dir / Path(file_name).stem
+
+            print("Processing file", text_file)
+            file_content = Path(text_file).read_text()[:50]
+            call_api_tts_and_save(file_content, audio_file)
+
+
+def call_api_tts_and_save(text: str, audio_file_name: str | Path) -> Path:
+    body = {
+        "language": "fr",
+        "text": text
+    }
+    response = requests.post(posixpath.join(API_BASE_URL, "tts"), json=body)
+    response.raise_for_status()
+    json_content = response.json()
+
+    audio_file_path = audio_file_name.absolute().as_uri() + ".wav"
+    print("Saving audio file to", audio_file_path)
+
+    Path(audio_file_path).parent.mkdir(parents=True, exist_ok=True)
+    scipy.io.wavfile.write(
+        audio_file_path,
+        rate=json_content["rate"],
+        data=np.array(json_content["data"])
+    )
+    return Path(audio_file_path)
+
+
+if __name__ == '__main__':
+    main()
